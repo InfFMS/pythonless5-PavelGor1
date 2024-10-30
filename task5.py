6f02e46fd5638fc6d2078385349ec94653408761
PavelGor1 <116425791+PavelGor1@users.noreply.github.com>
2024-10-30 11:55:21 +0700

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,6 +1,5 @@
 # Введите массив длины N с клавиатуры и найдите (за один проход)
 # количество элементов, имеющих максимальное значение.
-<<<<<<< HEAD
 from random import randint
 n = int(input())
 mas = [randint(0, 1000)for i in range(n)]
@@ -12,9 +11,7 @@ for i in range(n):
     if a == mas[i]:
         s += 1
 print(s,'- кол-во совпадающих чисел')
-=======
 mas = [1,2,3,4]
 for i in range(2):
     mas.reverse()
-print(mas)
->>>>>>> 8faa1b3e8d72c09201fa3262514ed4bfd49bc206
+print(mas) 
